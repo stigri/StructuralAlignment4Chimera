60e0c69c71ac54cfc2aec6e3b10a58c7e17d0004
Stine Griep <stine.griep@googlemail.com>
2014-12-08 21:43:01 +0100

    Print all atoms and atom properties of all open models (proteins)

--- startupScript.py
+++ startupScript.py
@@ -1,3 +1,16 @@
-# Just print the currently open models
-print chimera.openModels.list(modelTypes = [chimera.Molecule])
+import chimera
+
+# Store all currently open models in the variable called "models"
+models = chimera.openModels.list(modelTypes = [chimera.Molecule])
+
+# Loop over all atoms of all open models and print the atom's coordinates
+for p in models:
+    print p, "\n", "#####"
+    for a in p.atoms:
+	print "Name:", a.name
+	print "Coord:", a.coord()
+	print "xform:", a.xformCoord()
+	print "ID ATM Type:", a.idatmType
+	print "Residue:",  a.residue.type, a.residue.id
+	print "--------"
 
