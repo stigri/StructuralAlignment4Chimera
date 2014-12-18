610878d960b4a464e47e3f58bf48ea1d5f9efae8
Stine Griep <stine.griep@googlemail.com>
2014-12-18 13:23:41 +0100

    Fix inconsistent indentation

--- __init__.py
+++ __init__.py
@@ -2,24 +2,25 @@ import chimera
 import wurstl
 from chimera import replyobj
 
+
 def startup():
     # Store all currently open Chimera models in the variable called "models"
     models = chimera.openModels.list()
 
     # Need at least to models to work with (technically we need exactly two).
     if len(models) < 2:
-	replyobj.error("Cannot create alignment out of less than two models.")
-	return
+        replyobj.error("Cannot create alignment out of less than two models.")
+        return
 
     # Loop over all atoms of all open models and convert Chimera's
     # data model into a user-type (model, atom, residue) independent
     # representation solely based on Python primitives. This can be
-    # passed to wurstl and further converted to C data types.
+    # passed to wurstl and further converted to C dat   a types.
     #
     # The resulting model data structure looks like:
     #
     # ["compnd first model",
-    #         [("0", "idamtype", ..., x, y, z),
+    #   [("0", "idamtype", ..., x, y, z),
     #          (second atom), 
     #          ...,
     #          (n-th atom)
@@ -33,26 +34,26 @@ def startup():
     #
     model = []
     for p in models:
-	model.append(p.name)
+        model.append(p.name)
 
-	# convert all atoms of the molecule into a list of atoms
+        # convert all atoms of the molecule into a list of atoms
         atoms = []
         for a in p.atoms:
-		atom = []
-		atom.append(a.name) # str
-		atom.append(a.idatmType) # str
-		atom.append(a.residue.type) # str
-		atom.append(a.residue.id.insertionCode) # str
-		atom.append(a.residue.id.chainId) # str
-		atom.append(a.residue.id.position) # int
-		atom.append(a.coord().x) # float
-		atom.append(a.coord().y) # float
-		atom.append(a.coord().z) # float
+            atom = []
+            atom.append(a.name)  # str
+            atom.append(a.idatmType)  # str
+            atom.append(a.residue.type)  # str
+            atom.append(a.residue.id.insertionCode)  # str
+            atom.append(a.residue.id.chainId)  # str
+            atom.append(a.residue.id.position)  # int
+            atom.append(a.coord().x)  # float
+            atom.append(a.coord().y)  # float
+            atom.append(a.coord().z)  # float
 
-		# stuff list of atom properties into list of atoms
-		atoms.append(atom)
+            # stuff list of atom properties into list of atoms
+            atoms.append(atom)
 
-	model.append(atoms)
+        model.append(atoms)
 
     # wurstl returns a list of pairs. Each pair has the indices
     # of an atom in the first model aligned to an atom in the
@@ -69,18 +70,20 @@ def startup():
     mobileAtoms = []
     referenceAtoms = []
     for alignment in alignments:
-		idxA = models[0].atoms[alignment[0]]
-		idxB = models[1].atoms[alignment[1]]
-		if idxA != -1 and idxB != -1:
-			mobileAtoms.append(idxA)
-			referenceAtoms.append(idxB)
+        idxA = models[0].atoms[alignment[0]]
+        idxB = models[1].atoms[alignment[1]]
+        # ignore GAP_INDEX entries
+        if idxA != -1 and idxB !=  -1:
+            mobileAtoms.append(idxA)
+            referenceAtoms.append(idxB)
 
     # Use Midas' match command to visually align
     # both models.
     # see https://www.cgl.ucsf.edu/chimera/docs/UsersGuide/midas/match.html
     from Midas import match, TooFewAtomsError
+
     try:
-	match(mobileAtoms, referenceAtoms, None)
+        match(mobileAtoms, referenceAtoms, None)
     except TooFewAtomsError:
-	replyobj.error('Too few corresponding atoms (<4) to '
-		'match both models\n')
+        replyobj.error('Too few corresponding atoms (<4) to '
+                       'match both models\n')
